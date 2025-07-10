3b064104b5d2dea1725284cdb2a4c3fde86810da
Cintia Cabral <cintiacabral@hotmail.com>
2025-07-10 09:54:17 -0300

    Accessing elements with DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("form")
+const apiKeyInput = document.getElementById('apiKey')
+const gameSelect = document.getElementById('gameSelector')
+const questionInput = document.getElementById("questionInput")
+const askButton = document.getElementById("askButton")
+const aiResponse = document.getElementById("aiResponse")
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+})
